9e346dbc3c58f4f8bcc8ffe78cd24d85e2b84fba
MiragemS <musantos016@gmail.com>
2025-03-01 20:23:51 -0300

    Ordenação em List

---
+++ collections-java-api-dio-learning/src/OrdenacaoLista/Main.java
@@ -0,0 +1,15 @@
+package OrdenacaoLista;
+
+public class Main {
+    public static void main(String[] args) {
+        OrdenacaoPessoa ordena = new OrdenacaoPessoa();
+
+        ordena.adicionarPessoa("Nome 1", 12, 1.45);
+        ordena.adicionarPessoa("Nome 2", 18 , 1.85);
+        ordena.adicionarPessoa("Nome 3", 7, 1.10);
+        ordena.adicionarPessoa("Nome 4", 19, 1.70);
+
+        System.out.println(ordena.ordenarPorAltura());
+        System.out.println(ordena.ordenarPorIdade());
+    }
+}
---
+++ collections-java-api-dio-learning/src/OrdenacaoLista/OrdenacaoPessoa.java
@@ -0,0 +1,29 @@
+package OrdenacaoLista;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoPessoa{
+    private List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoa(){
+        pessoaList = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(String nome, int idade, double altura){
+        pessoaList.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade(){
+        List<Pessoa> ordenadoPorIdade = new ArrayList<>(pessoaList);
+        Collections.sort(ordenadoPorIdade);
+        return ordenadoPorIdade;
+    }
+
+    public List<Pessoa> ordenarPorAltura() {
+        List<Pessoa> ordenadoPorAltura = new ArrayList<>(pessoaList);
+        Collections.sort(ordenadoPorAltura, new ComparatorPorAltura());
+        return ordenadoPorAltura;
+    }
+}
